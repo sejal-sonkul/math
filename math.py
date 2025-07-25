0e3d6f666ef847f134aa841e9243327328eb7abc
sejal-sonkul <sonkulsejal@gmail.com>
2025-07-25 16:00:27 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,16 @@
+N1 = int(input("Enter the first number: "))
+N2 = int(input("Enter the second number: "))
+
+add = N1 + N2
+
+sub = N1 - N2
+
+mul = N1 * N2
+
+div = N1 / N2
+
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multipication: ",mul)
+print("Division: ",div)
+
